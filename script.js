8e0e3f6650124f8b68cb49a4af59e93ea4b9586d
lubnarimi612-debug <lubnarimi612@gmail.com>
2026-02-24 14:26:53 +0600

    add calculation logic and tab toggle functionality

--- script.js
+++ script.js
@@ -18,3 +18,60 @@ const filteredSection = document.getElementById('filtered-section');
 const allCardSection = document.getElementById('all-cards');
 
 const mainContainer = document.querySelector('main');
+
+// count calculate machine
+function calculateCount() {
+  const currentTotal = allCardSection.children.length;
+  total.innerText = currentTotal;
+
+  if (currentStatus === 'interview-filter-btn') {
+    countJobs.innerText = `${interviewList.length} of ${currentTotal}`;
+  }
+  else if (currentStatus === 'rejected-filter-btn') {
+    countJobs.innerText = `${rejectedList.length} of ${currentTotal}`;
+  }
+  else {
+    countJobs.innerText = currentTotal;
+  }
+
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+}
+
+// machine call
+calculateCount();
+
+
+function toggleStyle(id) {
+  // class remove
+  allFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+  interviewFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+  rejectedFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white')
+
+  // class added
+  allFilterBtn.classList.add('bg-white', 'text-[#64748B]')
+  interviewFilterBtn.classList.add('bg-white', 'text-[#64748B]')
+  rejectedFilterBtn.classList.add('bg-white', 'text-[#64748B]')
+
+  const selected = document.getElementById(id);
+  currentStatus = id;
+  selected.classList.remove('bg-white', 'text-[#64748B]')
+  selected.classList.add('bg-[#3B82F6]', 'text-white')
+  
+  // sorto anujai section show hide
+  if (id === 'interview-filter-btn') {
+    allCardSection.classList.add('hidden');
+    filteredSection.classList.remove('hidden');
+    renderInterview();
+  } else if (id === 'all-filter-btn') {
+    allCardSection.classList.remove('hidden');
+    filteredSection.classList.add('hidden');
+  } else if (id === 'rejected-filter-btn') {
+    allCardSection.classList.add('hidden');
+    filteredSection.classList.remove('hidden');
+    renderRejected();
+  }
+  // machine call
+  calculateCount();
+
+}
